024b4a01e1cd9c912da0483a14c5d1422318dac7
gabrielgouvertte <gfgouvertte@gmail.com>
2024-08-30 12:45:33 -0300

    Rotas do chat e chamados

--- index.js
+++ index.js
@@ -47,6 +47,27 @@ app.get('/chamados/list', async (req, res) => {
     }
 })
 
+app.get('/chamados/search/:id', async (req, res) => {
+    try {
+        const [rows, fields] = await connection.execute(
+            `SELECT * FROM chamados WHERE id = ${req.params.id}` 
+        )
+        
+        console.log(rows)
+        
+        res.status(202).json({
+            success: true,
+            content: rows
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: true,
+            message: error,
+        });
+    }
+})
+
 app.post('/chamados/add', async (req, res) => {
     const data = req.body
     try {
@@ -60,6 +81,93 @@ app.post('/chamados/add', async (req, res) => {
         res.status(202).json({
             success: true,
             message: "Registro criado"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: error,
+        });
+    }
+})
+
+app.put('/chamados/update', async (req, res) => {
+    const data = req.body
+
+    try{
+        const [rows, fields] = await connection.execute(
+            `UPDATE chamados 
+            SET 
+                responsavel = '${data["responsavel"]}',
+                setor = '${data["setor"]}',
+                tecnico = '${data["tecnico"]}',
+                tags = '${data["tags"]}',
+                status = '${data["status"]}',
+                linksBase = NULL,
+                edited_at = current_timestamp(),
+                edited_by = 'SEM NOME AINDA'
+            WHERE 
+                chamados.id = ${data["id"]}`
+        )
+
+        res.status(202).json({
+            success: true,
+            message: "Usuário logado"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: error,
+        });
+    }
+})
+
+app.put('/chamados/msgUpdate', async (req, res) => {
+    const data = req.body
+    
+    try{
+        const [rows, fields] = await connection.execute(
+            `SELECT mensagens FROM chamados WHERE id = ${data["id"]}` 
+        )
+        let temp = JSON.parse(rows[0]["mensagens"])
+        temp.push(data.mensagem)
+            // console.log JSON.parse(
+        await connection.execute(
+            `UPDATE chamados 
+            SET 
+                mensagens = '${JSON.stringify(temp)}'
+            WHERE 
+                chamados.id = ${data["id"]}`
+        )
+
+        const [rowsUP, fieldsUP] = await connection.execute(
+            `SELECT mensagens FROM chamados WHERE id = ${data["id"]}` 
+        )
+
+        res.status(202).json({
+            success: true,
+            msgData: rowsUP,
+            message: "Usuário logado"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: error,
+        });
+    }
+})
+
+app.delete('/chamados/delete/:id', async (req, res) => {
+    try{
+        const [rows, fields] = await connection.execute(
+            `DELETE FROM chamados WHERE chamados.id = ${req.params.id}`
+        )
+        
+        res.status(202).json({
+            success: true,
+            message: "Chamado removido com sucesso"
         })
     } catch (error) {
         console.log(error)
